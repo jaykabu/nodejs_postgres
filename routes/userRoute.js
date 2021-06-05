ad5ceaeb571c14b3fb9e9db73691e0d18637a28e
jaykabu <59605424+jaykabu@users.noreply.github.com>
2021-06-05 14:09:33 +0530

    protect route with jwt

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controller/userController');
+const checkAuth = require('../middleware/check-auth');
 
 router.post('/', UserController.create_user);
-router.get('/', UserController.get_all_user);
+router.get('/', checkAuth, UserController.get_all_user);
 router.get('/:userId', UserController.get_user_byId);
 router.patch('/:userId', UserController.update_user);
-router.delete('/:userId', UserController.delete_user);
+router.delete('/:userId', checkAuth, UserController.delete_user);
 
 //Login
 router.post('/login', UserController.login_user);
